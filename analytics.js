c33ffeea6a3f466d90d9f63707b51511e6c30b7f
Tomaj Bayat <btomaj@gmail.com>
2012-12-20 22:28:29 +1100

    Initial commit

---
+++ analytics.js
@@ -0,0 +1,158 @@
+/*jslint browser: true, plusplus: true */
+/*global console */
+/**
+ * @preserve Voyeur
+ * github.com/btomaj/voyeur
+ *
+ * All rights reserved.
+ */
+/**
+ * Include this file by inserting this snippet at the top of the <head />
+ * element of the document:
+ *
+ * <script type="text/javascript">
+ * var A = A || {};
+ * A.site = A.site || '<name>'; // site/platform/snippet identifier
+ * A.page = A.page || '<page>'; // page name when not using URL structure
+ * A.onLoad = function onLoad() { // called when implementation file has loaded
+ *     // ...opportunity to pass information to implementation using A.observer
+ * };
+ *
+ * (function () {
+ *     'use strict';
+ *
+ *     var timeStamp = new Date(),
+ *         cacheBuster = timeStamp.getUTCFullYear().toString() +
+ *             (timeStamp.getUTCMonth() + 1) +
+ *             timeStamp.getUTCDate() + 'T' +
+ *             timeStamp.getUTCHours() +
+ *             Math.ceil(timeStamp.getUTCMinutes() / 15) * 15,
+ *
+ *         hook = document.getElementsByTagName('script')[0],
+ *         script = document.createElement('script');
+ *
+ *     script.src = '//remotehost/analytics.js?cb=' + cacheBuster;
+ *     script.type = 'text/javascript';
+ *     script.async = true;
+ *
+ *     hook.parentNode.insertBefore(script, hook);
+ * }());
+ * </script>
+ *
+ * Appending analytics=<mode> to the query string allows you to dynamically
+ * select the source from which to load the implementation file to assist in
+ * debugging and development. The available <mode> options are:
+ *  - rc: loads the development file from the server.
+ *  - dev: loads the development file from localhost.
+ *  - restore: restores mode and loads the production file from the server.
+ * It is expected that the development file is named dev.js. The name of the
+ * production file needs to be defined in the analytics.js file. It is
+ * recommended that a naming convention using the release date is used to
+ * distinguish between releases (e.g. 20121212.js).
+ *
+ * The selected mode is preserved across page views by setting a session cookie.
+ * NB: This cookie is also used in the implementation file, if the cookie name
+ * is changed the change needs to be reflected in the implemetation file.
+ *
+ * TODO Document stubbing console.log()
+ * TODO Document defining A.file and host.
+ * TODO Document asychronous nature and how to use with any platform
+ *
+ * @module Analytics
+ * @main Analytics
+ */
+/**
+ * @class A
+ *
+ * @static
+ */
+var A = A || {};
+
+/**
+ * @property file
+ * @type String
+ */
+A.file = '<date>.js';
+
+(function () {
+    'use strict';
+
+    var host = '//remotehost/',
+        file = A.file,
+
+        query = window.location.search.substr(1).split('&'),
+        i = query.length,
+
+        mode;
+
+    if (!window.console) {
+        window.console = {
+            log: function () {}
+        };
+    }
+
+    while (i--) {
+        if (query[i].indexOf('analytics=') + 1) {
+            mode = query[i].replace('analytics=', '');
+
+            document.cookie = 'A-mode=' + mode + ';'; // preserve mode for subsequent pageviews; do not alter cookie name without ammending implementation.js
+            break;
+        }
+    }
+
+    if (!mode) {
+        query = document.cookie.split('; ');
+        i = query.length;
+
+        while (i--) {
+            if (query[i].indexOf('A-mode=') + 1) {
+                mode = query[i].replace('A-mode=', '');
+                break;
+            }
+        }
+    }
+
+    switch (mode) {
+    case 'dev':
+        host = 'http://localhost/';
+        file = 'dev.js?cb=' + Math.floor(1000000 * Math.random());
+
+        console.log('Development mode: Development file loaded from localhost');
+        break;
+
+    case 'rc':
+        file = 'dev.js?cb=' + Math.floor(1000000 * Math.random());
+
+        console.log('Release Candidate mode: Development file loaded from server');
+        break;
+
+    case 'restore':
+        document.cookie = 'A-mode=' + mode + '; Expires=' +
+            (new Date(0)).toGMTString() + ';';
+
+        console.log('Restore mode: Production file loaded from server');
+        break;
+
+    default:
+    }
+
+    mode = document.createElement('script');
+    mode.src = host + file;
+    mode.type = 'text/javascript';
+    mode.async = true;
+
+    if (mode.readyState) { // IE
+        mode.onreadystatechange = function () {
+            if (mode.readyState === 'loaded' || mode.readyState === 'complete') {
+                mode.onreadystatechange = undefined;
+                A.onLoad();
+            }
+        };
+    } else {
+        mode.onload = A.onLoad;
+    }
+
+    query = document.getElementsByTagName('script')[0];
+    query.parentNode.insertBefore(mode, query);
+
+}());
---
+++ implementation.js
@@ -0,0 +1,413 @@
+/*jslint browser: true, plusplus: true */
+/**
+ * @preserve Voyeur
+ * github.com/btomaj/voyeur
+ *
+ * All rights reserved.
+ */
+/**
+ * Startup process:
+ * 1. If page is internal, fire 'internal' event and continue,
+ * 2. If 'dev' or 'rc' mode is active, fire 'development' event and exit,
+ * 3. Else, If site is excluded, exit,
+ * 4. Else, If site name and domain are known,
+ *  4.1 If no development keywords are found in the URL, fire 'production' event
+ *      and exit,
+ *  4.2 If development keywords are found in the URL, fire 'development' event
+ *      and exit,
+ * 5. Else, unknown site and/or domain, do nothing and exit.
+ *
+ * @module Analytics
+ * @submodule Implementation
+ */
+var A = A || {};
+
+/**
+ * internal // domains that definitively identify browser as internal (e.g. intranet) useful to avoid retargeting internals
+ * exclude // domains that are not supported
+ * dev // development keywords in URL, e.g. 'staging', 'test', etc.
+ * @property config
+ * @type Object
+ */
+A.config = {
+    internal: [],
+    exclude: [],
+    site: [],
+    domain: [],
+    dev: []
+};
+
+/**
+ * Adobe Site Catalyst
+ */
+A.sc = A.sc || {};
+
+/**
+ * Google Analytics
+ */
+A.ga = A.ga || {};
+
+/**
+ * Kiss Metrics
+ */
+A.km = A.km || {};
+
+/**
+ * @preserve Marathon
+ * github.com/btomaj/marathon
+ *
+ * A standalone implementation of jQuery.ready(). The below copyright notice
+ * applies to the adaptation of the jQuery.ready() implementation only.
+ *
+ * Copyright 2012 jQuery Foundation and other contributors
+ * http://jquery.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/**
+ * Guarantees that the supplied callback is run but only run when the document
+ * has loaded.
+ *
+ * @method run
+ * @for A
+ *
+ * @param callback {Function} The function that must only be called after the
+ *      the document has loaded.
+ *
+ * @static
+ */
+A.run = (function () {
+    'use strict';
+
+    // Dependencies
+    var
+
+    // Private properties
+        backlog = [],
+        documentElement, // used for IE
+
+    // Private methods
+        run = function run(callback) {
+
+            if (!backlog) {
+                callback();
+            } else {
+                backlog.push(callback);
+            }
+
+        },
+
+        runBacklog = function runBacklog() {
+
+            if (!backlog) {
+                return;
+            }
+
+            if (!document.body) { // ensure <body> has loaded to confirm loading is complete
+                return setTimeout(runBacklog, 1);
+            }
+
+            var callback = backlog,
+                i = callback.length;
+
+            backlog = undefined;
+
+            while (i--) {
+                callback[i]();
+            }
+
+        },
+
+        DOMContentLoaded = function DOMContentLoaded() {
+
+            if (document.addEventListener) {
+                document.removeEventListener('DOMContentLoaded', DOMContentLoaded, false);
+                runBacklog();
+            } else if (document.readyState === 'complete') { // IE
+                document.detachEvent('onreadystatechange', DOMContentLoaded);
+                runBacklog();
+            }
+
+        };
+    // End var
+
+    // Initialisation proceedures
+    if (document.readyState === 'complete') { // catch cases where the browser ready event already fired
+
+        runBacklog();
+
+    } else if (document.addEventListener) { // standards compliant browsers
+
+        document.addEventListener('DOMContentLoaded', DOMContentLoaded, false);
+        window.addEventListener('load', runBacklog, false); // window.onload will always fire, use as fallback
+
+    } else { // fallback to event model
+
+        document.attachEvent('onreadystatechange', DOMContentLoaded); // iframe-safe fallback, fires before onload
+        window.attachEvent('onload', runBacklog); // window.onload will always fire, use as second fallback
+
+        // main test for IE when not a frame
+        // using trick by Diego Perini http://javascript.nwbox.com/IEContentLoaded/
+        documentElement = false;
+
+        try {
+            documentElement = window.frameElement === null && document.documentElement;
+        } catch (e) {}
+
+        if (documentElement && documentElement.doScroll) {
+
+            (function checkScroll() {
+
+                if (!backlog) {
+                    return;
+                }
+
+                try {
+                    documentElement.doScroll('left');
+                } catch (e) {
+                    return setTimeout(checkScroll, 49);
+                }
+
+                runBacklog();
+
+            }());
+
+        }
+
+    }
+
+    // Public API
+    return run;
+
+}());
+
+/**
+ * @preserve Big Brother
+ * github.com/btomaj/big-brother
+ *
+ * All rights reserved.
+ */
+/**
+ * A simple JavaScript observer
+ *
+ * @class observer
+ * @for A
+ *
+ * @static
+ */
+A.observer = (function () {
+    'use strict';
+
+    // Dependencies
+    var
+
+    // Private properties
+        registered = {},
+
+    // Private methods
+        /**
+         * Registers a new event handler for the specified event type.
+         *
+         * @method on
+         *
+         * @param {String} eventType
+         * @param {Function} eventHandler
+         *
+         * @static
+         */
+        on = function on(eventType, eventHandler) {
+            if (!registered[eventType]) {
+                registered[eventType] = [];
+            }
+
+            registered[eventType].push(eventHandler);
+        },
+
+        /**
+         * Triggers the specified event type and passes the supplied custom
+         * event object to any registered event handlers.
+         *
+         * @method trigger
+         *
+         * @param {String} eventType
+         * @param {Object} eventObject
+         *
+         * @static
+         */
+        /**
+         * An alias to the trigger method. For consistency, consult the
+         * documentation for trigger method.
+         *
+         * @method fire
+         *
+         * @param {String} eventType
+         * @param {Object} eventObject
+         *
+         * @static
+         *
+         * @for A
+         */
+        trigger = function trigger(eventType, eventObject) {
+            var eventHandler = registered[eventType] || [],
+                i = eventHandler.length;
+
+            while (i--) {
+                eventHandler[i](eventObject);
+            }
+        };
+
+    // End var
+
+    // Initialisation proceedures
+
+    // Public API
+    return {
+        on: on,
+        trigger: trigger,
+        fire: trigger
+    };
+
+}());
+
+/**
+ * Inserts a beacon after the page has finished loading.
+ *
+ * interface Beacon {
+ *      type {String} The beacon type. Currently only 'img' is supported.
+ *      src {String} The location of the beacon.
+ * }
+ *
+ * @method beacon
+ * @for A
+ *
+ * @param insert {Object} Implements interface Beacon (see method
+ *      documentation).
+ */
+A.beacon = function beacon(insert) {
+    'use strict';
+
+    var tag,
+        hook;
+
+    switch (insert.type) {
+    case 'img':
+        tag = document.createElement('img');
+        tag.src = insert.src;
+        tag.width = 1;
+        tag.height = 1;
+        tag.style.position = 'absolute';
+        break;
+    default:
+    }
+
+    if (tag) {
+        A.run(function () {
+            hook = document.getElementsByTagName('script')[0];
+            hook.parentNode.insertBefore(tag, hook);
+        });
+    }
+};
+
+
+// ---- everything below this line should be the last thing in this file ---- //
+(function () {
+    'use strict';
+
+    // rewrite site names (A.site) if neccessary...
+    // rewrite page names (A.page) if neccessary...
+}());
+
+A.run(function () { // the shot caller (what a baller)
+    'use strict';
+
+    var config = A.config,
+
+        internal = config.internal,
+        exclude = config.exclude,
+        site = config.site,
+        domain = config.domain,
+        dev = config.dev,
+
+        query,
+        i,
+
+        mode;
+
+    query = document.domain;
+    i = internal.length;
+    while (i--) {
+        if (query.indexOf(internal[i]) + 1) {
+            A.observer.fire('internal');
+        }
+    }
+
+    query = document.cookie.split('; ');
+    i = query.length;
+    while (i--) {
+        if (query[i].indexOf('A-mode=') + 1) {
+            mode = query[i].replace('A-mode=', '');
+            break;
+        }
+    }
+    if (mode === 'dev' || mode === 'rc') {
+        A.observer.fire('development');
+        return;
+    }
+
+    query = document.domain;
+    i = exclude.length;
+    while (i--) {
+        if (query.indexOf(exclude[i]) + 1) {
+            return;
+        }
+    }
+
+    // query = document.domain;
+    i = dev.length;
+    while (i--) {
+        if (query.indexOf(dev[i]) + 1) {
+            dev = true;
+            break;
+        }
+    }
+    //query = document.domain;
+    i = domain.length;
+    while (i--) {
+        if (query.indexOf(domain[i]) + 1) {
+            domain = true;
+            break;
+        }
+    }
+    query = A.site;
+    i = site.length;
+    while (i--) {
+        if (site[i] === query) {
+            site = true;
+            break;
+        }
+    }
+    if (site === true && domain === true) {
+        if (dev === false) {
+            A.observer.fire('production');
+        } else {
+            A.observer.fire('development');
+        }
+    }
+
+});
