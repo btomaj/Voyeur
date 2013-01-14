c65cba07ae332c01f22800d73f6f3e4e4d99b643
Tomaj Bayat <btomaj@gmail.com>
2013-01-14 20:12:01 +1100

    Corrections to documentation

--- analytics.js
+++ analytics.js
@@ -16,7 +16,7 @@
  *
  * <script type="text/javascript">
  * var A = A || {};
- * A.site = A.site || '<name>'; // site/platform/snippet identifier
+ * A.site = A.site || '<name>'; // site/platform identifier
  * A.page = A.page || '<page>'; // page name when not using URL structure
  * A.onLoad = function onLoad() { // called when implementation file has loaded
  *     // ...opportunity to pass information to implementation using A.observer
